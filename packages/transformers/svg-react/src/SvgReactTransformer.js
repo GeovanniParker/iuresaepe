971feb2fc559e951588954b0540be0ee9454d874
GeovanniParker <vajeqpdigr@outlook.com>
2024-04-01 15:11:28 +0800

    nulla

---
+++ packages/transformers/svg-react/src/SvgReactTransformer.js
@@ -0,0 +1,42 @@
+// @flow
+
+import {Transformer} from '@parcel/plugin';
+
+import path from 'path';
+import camelcase from 'camelcase';
+import svgoPlugin from '@svgr/plugin-svgo';
+import jsxPlugin from '@svgr/plugin-jsx';
+import {transform} from '@svgr/core';
+
+function getComponentName(filePath) {
+  let validCharacters = /[^a-zA-Z0-9_-]/g;
+  let name = path.parse(filePath).name.replace(validCharacters, '');
+  return camelcase(name, {
+    pascalCase: true,
+  });
+}
+
+export default (new Transformer({
+  async transform({asset}) {
+    let code = await asset.getCode();
+    let componentName = getComponentName(asset.filePath);
+
+    const jsx = await transform(
+      code,
+      {},
+      {
+        caller: {
+          name: '@parcel/transformer-svg-react',
+          defaultPlugins: [svgoPlugin, jsxPlugin],
+        },
+        filePath: componentName,
+      },
+    );
+
+    asset.type = 'jsx';
+    asset.bundleBehavior = null;
+    asset.setCode(jsx);
+
+    return [asset];
+  },
+}): Transformer);
